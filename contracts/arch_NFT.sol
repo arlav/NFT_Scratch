ff9e8d9c36b89688c7bdbbdfa0e1a54ad857060e
arlav <dounas@gmail.com>
2022-11-07 20:28:24 +0000

    Changes to be committed: 	modified:   brownie-config.yaml 	modified:   contracts/arch_NFT.sol

--- contracts/arch_NFT.sol
+++ contracts/arch_NFT.sol
@@ -1,3 +1,9 @@
 pragma solidity 0.6.6;
 
-import "@openzeppelin/contracts/token/ERC721.ERC721.sol"
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+
+contract ArchNFT is ERC721, VRFConsumerBase {
+
+}
