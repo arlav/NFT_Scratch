08d22d61414999e1e9abfb39e050da8928416aa7
arlav <dounas@gmail.com>
2022-11-08 20:55:25 +0000

    modified:   contracts/arch_NFT.sol

--- contracts/arch_NFT.sol
+++ contracts/arch_NFT.sol
@@ -6,4 +6,21 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract ArchNFT is ERC721, VRFConsumerBase {
 
+
+    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash) public 
+    VRFConsumer(_VRFCoordinator, _LinkToken)
+    ERC721("ArchNFT", "ARCH")
+    {
+
+    }
+
+    //@dev tokenURI might point to an API as well
+    //@dev this creates the NFT token
+    function createCollectible(uint256 userProvidedSeed, string memory tokenURI)
+    public returns (bytes32) 
+    {
+
+    }
+
+
 }
